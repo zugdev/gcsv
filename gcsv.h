5e5b27d3840345fa349338ec308a8a9d9a9c2d5f
zugdev <zug.dev@proton.me>
2024-10-29 19:07:26 -0300

    feat: init gcsv

---
+++ gcsv.h
@@ -0,0 +1,7 @@
+#ifndef GCSV_H
+#define GCSV_H
+
+void gcsv_compress(const char *input_file, const char *output_file);
+void gcsv_decompress(const char *input_file, const char *output_file);
+
+#endif
