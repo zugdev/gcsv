fe2249a94833b5ac5fb11e3267c489fc4c908d1c
zugdev <zug.dev@proton.me>
2024-10-29 23:01:59 -0300

    feat: deprecate C code

--- gcsv.h
+++
@@ -1,7 +0,0 @@
-#ifndef GCSV_H
-#define GCSV_H
-
-void gcsv_compress(const char *input_file, const char *output_file);
-void gcsv_decompress(const char *input_file, const char *output_file);
-
-#endif
